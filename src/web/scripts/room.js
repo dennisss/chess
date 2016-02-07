3466a720a3b02fa349ed163320e23dcd29cb7033
Dan Ziegler <dmz38@drexel.edu>
2016-02-07 13:29:37 -0500

    Refs #54 - Fixes issue with hitting the back button - Alert is no longer in an alert box but a nice alert UI - Centered our nyan cat because... well nyan cat should be centered!

--- src/web/scripts/room.js
+++ src/web/scripts/room.js
@@ -4,6 +4,7 @@ var roomName;
 
 function load(router){
 	page = $('#player-creation');
+	$("#playerProblems").hide();
 
 	var opName = "";
 	var opLevel = "";
@@ -14,7 +15,7 @@ function load(router){
 		if (element.name != $('#playerName').val()) {
 			$("#playerTableBody").append("<tr class='player'><td class='opName' data-opID='" + element.id + "'>" + element.name + "</td><td class='opLevel'>" + element.level + "</td></tr>");
 		} else if(array.length == 1) {
-			$("#playerTableBody").append("<tr><td colspan='2' class='text-center disabled'><img src='https://orig11.deviantart.net/dcab/f/2011/158/1/6/nyan_cat_by_valcreon-d3iapfh.gif' style='height: 150px; opacity: 0.4;'><br>Sorry!  There are no available players available at this time.  Hang in there for your friends or <a href='/r/lobby' id='exitLink'>click here</a> to go to the main lobby.<br></td></tr>");
+			$("#playerTableBody").append("<tr><td colspan='2' class='text-center disabled'><img src='https://orig11.deviantart.net/dcab/f/2011/158/1/6/nyan_cat_by_valcreon-d3iapfh.gif' style='height: 150px; opacity: 0.4; padding-right: 100px;'><br>Sorry!  There are no available players available at this time.  Hang in there for your friends or <a href='/r/lobby' id='exitLink'>click here</a> to go to the main lobby.<br></td></tr>");
 		} // https://49.media.tumblr.com/8210fd413c5ce209678ef82d65731443/tumblr_mjphnqLpNy1s5jjtzo1_400.gif
 	}
 
@@ -26,6 +27,8 @@ function load(router){
 
 	$('#btnChooseOp').click(function(){
 		if ($('#playerName').val().length > 0 && $('#experience').val() !== "Chose your experience...") {
+			$("#playerProblems").html("");
+			$("#playerProblems").hide();
 			thisPlayer = $('#playerName').val();
 			console.log( $('#experience').val());
 			$('#playerList').collapse();
@@ -33,24 +36,22 @@ function load(router){
 				$("#playerTableBody").html("");
 				data.forEach(printToTable);
 			});
-			/*
-			setTimeout(function() {
-				$("#challengeNotification").modal("show");
-
-				//router.go('game', { opName: opName, opLevel: opLevel, thisPlayer: thisPlayer });
-
-			}, 5000); */
-			//challengeNotification
 		} else {
-			alert('Please enter your name and choose your difficulty!');
+			$("#playerProblems").html("Please enter your name and choose your difficulty!");
+			$("#playerProblems").fadeIn();
+			//alert('Please enter your name and choose your difficulty!');
 		}
 	});
 
 	$('#btnRandomOp').click(function(){
 		if ($('#playerName').val().length > 0 && $('#experience').val() !== "Choose your experience...") {
+			$("#playerProblems").html("");
+			$("#playerProblems").hide();
 			$('#loadingPlayer').modal({ backdrop: 'static' });
 		} else {
-			alert('Please enter your name and choose your difficulty!');
+			$("#playerProblems").html("Please enter your name and choose your difficulty!");
+			$("#playerProblems").fadeIn();
+			//alert('Please enter your name and choose your difficulty!');
 		}
 	});
 
@@ -60,6 +61,11 @@ function load(router){
 
 		});
 		router.go('game', { opName: opName, opLevel: opLevel, thisPlayer: thisPlayer });
+	});
+
+	$("#joinGameBack").click(function(){
+		$("#player-creation").modal("hide");
+		$("#playerList").hide();
 	});
 
 	$("#challengedDenied").click(function(){
