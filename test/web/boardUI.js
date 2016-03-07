4889c355e50d595117bbdb3492912e6b856a0ac1
Tess <tess.distefano@gmail.com>
2016-03-06 23:17:14 -0500

    add tests for BoardUI

---
+++ test/web/boardUI.js
@@ -0,0 +1,100 @@
+describe('BoardUI', function(){
+
+	describe('BoardUi', function(){
+
+		describe('submitMove()', function(){
+
+			it.skip('state gets updated after move is submitted', function () {
+
+			});
+		});
+
+		describe('start()', function(){
+
+			it.skip('player is updated', function () {
+
+			});
+
+			it.skip('player state is updated to waiting', function () {
+
+			});
+
+			it.skip('player state is updated to current turn', function () {
+
+			});
+		});
+
+		describe('updatePlayer()', function(){
+
+			it.skip('current player is passed in color', function () {
+
+			});
+		});
+
+		describe('updateBoard()', function(){
+
+			it.skip('features of new board are present', function () {
+
+			});
+
+			it.skip('features of old board are not present', function () {
+
+			});
+		});
+
+		describe('updateState()', function(){
+
+			it.skip('state is set to passed in state', function () {
+
+			});
+		});
+
+		describe('getCell()', function(){
+
+			it.skip('get proper value of filled in cell', function () {
+
+			});
+
+			it.skip('get proper value of empty cell', function () {
+
+			});
+
+			it.skip('get proper value of invalid cell', function () {
+
+			});
+		});
+
+		describe('animateMove()', function(){
+
+			it.skip('ensure no timeout', function () {
+
+			});
+		});
+
+		describe('processMove()', function(){
+
+			it.skip('move is applied', function () {
+
+			});
+
+			it.skip('board is updated', function () {
+
+			});
+
+			it.skip('state is updated', function () {
+
+			});
+		});
+
+		describe('reset()', function(){
+
+			it.skip('board that used to be random is now default', function () {
+
+			});
+
+			it.skip('board that used to be default is now still default', function () {
+
+			});
+		});
+	});
+});
