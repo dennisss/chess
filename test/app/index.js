09baae92f2874e3dbafc09b624c2dabede11a754
Tess <tess.distefano@gmail.com>
2016-03-07 20:04:30 -0500

    add index tests of child processes

--- test/app/index.js
+++ test/app/index.js
@@ -1,4 +1,12 @@
-describe('App', function(){
+var child_process = require('child_process')
 
+describe('Index', function(){
+	it('can start and kill child processes', function *() {
+		this.timeout(100);
 
+		server = child_process.spawn('node', ['src/app'], {
+			stdio: ['ignore', 1, 2]
+		});
+		server.kill('SIGINT');
+	});
 });
